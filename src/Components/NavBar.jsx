38ac1fb592af5e286e48db78b4e55484f07fa45f
Federico Gallo <sfedericogallo@gmail.com>
2022-03-10 18:16:26 -0300

    contador y card

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,27 +4,124 @@ import CartWidget from "./CartWidget/CartWidget";
 const NavBar = () => {
   return (
     <div>
-      <div className="text-white">
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <nav className="navbar navbar-expand-lg navbar-light text-white bg-dark">
+        <div className="container-fluid">
+          <ul className="navbar-nav">
             <li className="nav-item dropdown">
               <a
-                className="nav-link dropdown-toggle"
-                href={}
-                id="navbarDropdown"
+                className="nav-link dropdown-toggle text-white"
+                href="#"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Guitarras
+                {" "}
+                Guitarras{" "}
               </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li>
+                  {" "}
+                  <a className="dropdown-item" href="#">
+                    {" "}
+                    Electricas{" "}
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a className="dropdown-item" href="#">
+                    {" "}
+                    Acusticas{" "}
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a className="dropdown-item" href="#">
+                    {" "}
+                    Electroacusticas{" "}
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle text-white"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                {" "}
+                Amplificadores{" "}
+              </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Valvulares
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Transistores{" "}
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    {" "}
+                    Amp. Acusticos{" "}
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle text-white"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                {" "}
+                Multiefectos{" "}
+              </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li>
+                  {" "}
+                  <a className="dropdown-item" href="#">
+                    {" "}
+                    Pedales{" "}
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a className="dropdown-item" href="#">
+                    {" "}
+                    Multiefectos{" "}
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a className="dropdown-item" href="#">
+                    {" "}
+                    Racks{" "}
+                  </a>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
-        >
         <CartWidget />
-        <ul />
-      </div>
+      </nav>
     </div>
   );
 };
