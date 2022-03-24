87e6c818ffe129a680438afd0f3f7beb1244f337
Federico Gallo <sfedericogallo@gmail.com>
2022-03-24 01:47:33 -0300

    Desafío de eventos

--- src/Components/Counter/ItemCounter.jsx
+++ src/Components/Counter/ItemCounter.jsx
@@ -1,24 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 
-const ItemCounter = () => {
-  const [contador, setContador] = useState;
-
+const ItemCounter = ({ max, contador, setContador, agregar }) => {
   const sumar = () => {
-    setContador(contador + 1);
+    contador < { max } && setContador(contador + 1);
   };
 
   const restar = () => {
-    setContador(contador - 1);
+    contador > 1 && setContador(contador - 1);
   };
 
   return (
-    <div>
-      <button className="btn btn-outline-primary" onClick={restar}>
+    <div className="p-1 text-center">
+      <button className="btn btn-outline-dark " onClick={restar}>
         -
       </button>
-      <span className="mx-3">contador</span>
-      <button className="btn btn-primary" onClick={sumar}>
+      <span className="mx-3 ">{contador}</span>
+      <button className="btn btn-dark " onClick={sumar}>
         +
+      </button>
+      <br />
+      <button
+        type="button"
+        className="btn btn-warning btn-lg d-grid mx-auto my-2"
+        onClick={agregar}
+      >
+        Comprar
       </button>
     </div>
   );
--- src/Components/Details/ItemDetail.jsx
+++ src/Components/Details/ItemDetail.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemCounter from "../Counter/ItemCounter";
 
-function ItemDetail({ guitarra }) {
+const ItemDetail = ({ guitarra }) => {
+  const [contador, setContador] = useState(0);
+
+  const agregar = () => {
+    const cart =
+      { 
+        {guitarra?.id}
+        {guitarra?.nombre}
+        {guitarra?.precio}
+        {guitarra?.pictureURL}
+        contador,
+       } 
+        console.log(cart);
+  };
+
   return (
     <>
-      <div className="card mb-3 max-width: 540px">
+      <div className="card mb-3 max-width:600px">
         <div className="row g-0">
-          <div className="col-md-4">
+          <div className="col-md-4 rounded">
             <img
               src={guitarra?.pictureURL}
               className="img-fluid rounded-start"
               alt={guitarra?.pictureURL}
             />
+            <ItemCounter
+              max={guitarra?.stock}
+              contdor={contador}
+              setContador={setContador}
+              agregar={agregar}
+            />
           </div>
+
           <div className="col-md-8">
             <div className="card-body">
               <h1 className="card-title">{guitarra?.marca}</h1>
@@ -23,8 +44,9 @@ function ItemDetail({ guitarra }) {
           </div>
         </div>
       </div>
+      <div></div>
     </>
   );
-}
+};
 
 export default ItemDetail;
