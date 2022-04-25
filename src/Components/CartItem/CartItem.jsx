390907eddbd96fbe1426e4b20c15e2f3b2614208
Federico Gallo <sfedericogallo@gmail.com>
2022-04-25 20:50:52 -0300

    Proyecto Final

--- src/Components/CartItem/CartItem.jsx
+++ src/Components/CartItem/CartItem.jsx
@@ -11,28 +11,28 @@ export const CartItem = ({ imagen, nombre, cantidad, precio, id }) => {
       <div className="row">
         <div className="col text-center">
           <div className="row">
-            <div className="col-sm-3 m-2">
+            <div className="col-sm-2 m-1">
               <img src={imagen} className="tamanio" alt="instrumento" />
             </div>
-            <div className="col-sm-3 m-2">
+            <div className="col-sm-3 m-1">
               <p>
                 <strong>Marca:</strong>
               </p>
               <p>{nombre}</p>
             </div>
-            <div className="col-sm-2 m-2">
+            <div className="col-sm-2 m-1">
               <p>
                 <strong>Cantidad:</strong>
               </p>
               <p>{cantidad}</p>
             </div>
-            <div className="col-sm-3 m-2">
+            <div className="col-sm-2 m-1">
               <p>
                 <strong>Precio U$S:</strong>
               </p>
               <p>{precio}</p>
             </div>
-            <div className="col-sm-1 m-2">
+            <div className="col-sm-2 m-1">
               <button
                 type="button"
                 className="btn btn-secondary"
